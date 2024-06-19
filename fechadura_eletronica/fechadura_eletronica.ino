91c4b52ec9aabd35b6f81c7f50e8eee377493580
Yuri David Silva Duarte <102871804+Boyuridod@users.noreply.github.com>
2024-06-19 10:31:12 -0300

    Update fechadura_eletronica.ino

--- fechadura_eletronica/fechadura_eletronica.ino
+++ fechadura_eletronica/fechadura_eletronica.ino
@@ -2,7 +2,11 @@
 #include <SPI.h>                                // Inclui a biblioteca SPI.h
 #include <Wire.h>                               // Inclui a biblioteca Wire.h
 #include <MFRC522.h>                            // Inclui a biblioteca do sensor RFID
-#include <ESP8266Firebase.h>                    // Inclui a biblioteca que faz conexão com Firebase
+#include <Firebase_ESP_Client.h>                    // Inclui a biblioteca que faz conexão com Firebase
+// Provide the token generation process info.
+#include <addons/TokenHelper.h>
+// Provide the RTDB payload printing info and other helper functions.
+#include <addons/RTDBHelper.h>
 
 // Definição das Portas
 
@@ -23,34 +27,58 @@ MFRC522::MIFARE_Key key;
 // Sensor magnético da porta
 #define Sensor_Porta D4                         // Fio do Sensor Magnético | O outro vai no GND
 
-// Firebase
-#define URL_Firebase "https://bancopi7-default-rtdb.firebaseio.com/"  // URL para o banco de dados Firebase
-Firebase firebase(URL_Firebase);
+// Definição de Constantes
 
-//Definição de Constantes
+const char* ssid = "Inside The Forest";         // Nome da rede
+const char* password = "youarealone";           // Senha da rede
 
-const char* ssid = "Inside The Forest";         //Nome da rede
-const char* password = "youarealone";           //Senha da rede
+// const char* ssid = "S23 Ultra de Vinicius Gabriel";         // Nome da rede
+// const char* password = "pcgkkdr9y54r5qn";                  // Senha da rede
 
-//Variáveis
+// Variáveis
 
 int portaIsFechada = 1;
 
-//Início do código
+// Conexão com Firebase
+
+/* 2. Define the API Key */
+#define API_KEY "AIzaSyBUiYGI7-oV9mmKp_6XHm5D2v9_zRVvJBU"
+
+/* 3. Define the RTDB URL */
+#define DATABASE_URL "https://bancodochaves-default-rtdb.firebaseio.com/" // <databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
+
+/* 4. Define the user Email and password that already registered or added in your project */
+#define USER_EMAIL "projetoint7@gmail.com"
+#define USER_PASSWORD "Senha@123456"
+
+// Variáveis Firebase
+
+// Define Firebase Data object
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long sendDataPrevMillis = 0;
+unsigned long count = 0;
+
+// Início do código
 
 void setup() {
-  Serial.begin(115200);                           //Inicia a comunicação com o Serial | Padrão: 9600 bps | Usando: 115200 bps
+  Serial.begin(115200);                           // Inicia a comunicação com o Serial | Padrão: 9600 bps | Usando: 115200 bps
 
   SPI.begin();         // Init SPI bus
   mfrc522.PCD_Init();  // Init MFRC522
   // Serial.println("Coloque sua tag RFID perto do leitor...");
   // Serial.println();
 
-  conecta_wifi();                               //Chama a função que irá conectar no WIFi
+  conecta_wifi();                               // Chama a função que irá conectar no WIFi
+
+  conecta_firebase();
 
   //Setando os pinos
-  pinMode(FechaduraPinA, OUTPUT);               //Configura D1 como saida
-  pinMode(FechaduraPinB, OUTPUT);               //Configura D2 como saida
+  pinMode(FechaduraPinA, OUTPUT);               // Configura D1 como saida
+  pinMode(FechaduraPinB, OUTPUT);               // Configura D2 como saida
   // pinMode(Sensor_Porta, INPUT_PULLUP);
 
   //Inicializando os pinos
@@ -62,72 +90,91 @@ void setup() {
 
 void loop() {
 
-  portaIsFechada = digitalRead(Sensor_Porta);
+  // Defina o caminho do nó
+  String path = "/ChavesCadastradas/-O-XhGQQqwWPdecsGw1-";
 
-  Serial.println(portaIsFechada);
-
-  // Look for new cards
-  if (mfrc522.PICC_IsNewCardPresent()){
-
-    // Select one of the cards
-    if (!mfrc522.PICC_ReadCardSerial()) {
-      return;
+  // Obtenha os dados do Firebase
+  if (Firebase.RTDB.getString(&fbdo, path + "/codChave")) {
+    if (fbdo.dataType() == "string") {
+      Serial.println("codChave: " + fbdo.stringData());
     }
-
-    String leitura = "";
-
-    // Show UID on serial monitor
-    Serial.print("UID da tag: ");
-    for (byte i = 0; i < mfrc522.uid.size; i++) {
-      if(i == 0)
-        leitura.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : ""));
-      else
-        leitura.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-
-      leitura.concat(String(mfrc522.uid.uidByte[i], HEX));
-    }
-
-    leitura.toUpperCase();
-
-    Serial.println(leitura);
-    
-    // Halt PICC
-    mfrc522.PICC_HaltA();
-    
-    // Stop encryption on PCD
-    mfrc522.PCD_StopCrypto1();
-
-    String query = "Tag/";
-
-    query.concat(leitura);
-    query.concat("/Nome");
-
-    Serial.println(query);
-    Serial.println(leitura);
-
-    String data = firebase.getString(query);
-    Serial.print("Received String: ");
-    Serial.println(data);
-
-    if(data != "ul"){
-
-      abre_fechadura();
-
-      firebase.setInt("isTrancada", false);
-
-      while(!portaIsFechada){
-
-        delay(1000);
-
-        portaIsFechada = digitalRead(Sensor_Porta);
-
-      }
-
-      fecha_fechadura();
-
-    }
-
+  } else {
+    Serial.println("Erro ao obter dados: " + fbdo.errorReason());
   }
+
+  // Aguarde antes de ler novamente
+  delay(5000);
+
+  // portaIsFechada = digitalRead(Sensor_Porta);
+
+  // Serial.println(portaIsFechada);
+
+  // if(Firebase.ready() && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0)){
+
+  //   sendDataPrevMillis = millis();
+
+    // // Look for new cards
+    // if (mfrc522.PICC_IsNewCardPresent()){
+
+    //   // Select one of the cards
+    //   if (!mfrc522.PICC_ReadCardSerial()) {
+    //     return;
+    //   }
+
+    //   String leitura = "";
+
+    //   // Show UID on serial monitor
+    //   Serial.print("UID da tag: ");
+    //   for (byte i = 0; i < mfrc522.uid.size; i++) {
+    //     if(i == 0)
+    //       leitura.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : ""));
+    //     else
+    //       leitura.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+
+    //     leitura.concat(String(mfrc522.uid.uidByte[i], HEX));
+    //   }
+
+    //   leitura.toUpperCase();
+
+    //   Serial.println(leitura);
+      
+    //   // Halt PICC
+    //   mfrc522.PICC_HaltA();
+      
+    //   // Stop encryption on PCD
+    //   mfrc522.PCD_StopCrypto1();
+
+    //   String query = "Tag/";
+
+    //   query.concat(leitura);
+    //   query.concat("/Nome");
+
+    //   Serial.println(query);
+    //   Serial.println(leitura);
+
+    //   //MANDA PRO BASE
+
+    //   if(False){ //COLOCAR A LOGICA DE VER SE TEM A CHAVE NO BANCO
+
+    //     abre_fechadura();
+
+    //     // firebase.setInt("isTrancada", false);
+
+    //     while(!portaIsFechada){
+
+    //       delay(1000);
+
+    //       portaIsFechada = digitalRead(Sensor_Porta);
+
+    //     }
+
+    //     fecha_fechadura();
+
+    //   }
+
+    // }
+
+  // }
   
 }
 
@@ -153,16 +200,52 @@ void conecta_wifi() {
 
 }
 
+void conecta_firebase() {
+
+  // Configuração do fb no void setup() - >
+  Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+
+  /* Assign the api key (required) */
+  config.api_key = API_KEY;
+
+  /* Assign the user sign in credentials */
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+
+  /* Assign the RTDB URL (required) */
+  config.database_url = DATABASE_URL;
+
+  /* Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
+
+  // Comment or pass false value when WiFi reconnection will control by your code or third party library e.g. WiFiManager
+  Firebase.reconnectNetwork(true);
+
+  // Large data transmission may require larger RX buffer, otherwise connection issue or data read time out can be occurred.
+  // Provide both RX and TX buffer sizes
+  fbdo.setBSSLBufferSize(4096, 4096); /* Rx buffer size in bytes from 512 - 16384 /, 1024 / Tx buffer size in bytes from 512 - 16384 */
+
+  // Limit the size of response payload to be collected in FirebaseData
+  fbdo.setResponseSize(2048);
+
+  Firebase.begin(&config, &auth);
+
+  Firebase.setDoubleDigits(5);
+
+  config.timeout.serverResponse = 10 * 1000;
+
+}
+
 void abre_fechadura() {
 
   digitalWrite(FechaduraPinA, HIGH);    //Configuração para abrir a fechadura
-  firebase.setString("isTrancada", "false");
+  // firebase.setString("isTrancada", "false");
 
 }
 
 void fecha_fechadura() {
 
   digitalWrite(FechaduraPinA, LOW);  //Configuração para fechar a fechadura
-  firebase.setString("isTrancada", "true");
+  // firebase.setString("isTrancada", "true");
 
 }
