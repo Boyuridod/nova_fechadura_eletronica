d4f6907f801ebe8c038f2e317924655db8703e1a
Vinicius Gabriel <viniciusribeiro449@gmail.com>
2024-06-20 11:25:19 -0300

    Novo código

--- sistema_porteiro/sistema_porteiro.ino
+++ sistema_porteiro/sistema_porteiro.ino
@@ -31,20 +31,9 @@ int portaIsFechada = 1;
 // Campainha
 #define Campainha D3
 
-
-
 // Para conexão com Wi-Fi
-
-// const char* ssid = "Inside The Forest";  // Nome da rede
-// const char* password = "youarealone";    // Senha da rede
-
-// const char* ssid = "S23 Ultra de Vinicius Gabriel";         // Nome da rede
-// const char* password = "pcgkkdr9y54r5qn";                  // Senha da rede
-
 const char* ssid = "BugWare";       // Nome da rede
 const char* password = "ej#bw@23";  // Senha da rede
-
-
 
 // Conexão com Firebase
 
@@ -69,59 +58,58 @@ FirebaseConfig config;
 unsigned long sendDataPrevMillis = 0;
 unsigned long count = 0;
 
-
-
 // Código
 
 void setup() {
-  Serial.begin(9600);  // Inicia a comunicação com o Serial | Padrão: 9600 bps | Usando: 115200 bps
+  Serial.begin(9600);  // Inicia a comunicação com o Serial
+  delay(100);          // Pequeno atraso para garantir que a serial está inicializada
 
-  Serial.println("Teste");
+  Serial.println("Inicializando...");
 
-  conecta_wifi();  // Chama a função que irá conectar no WIFi
+  conecta_wifi();  // Chama a função que irá conectar no Wi-Fi
 
-  Serial.println("Teste2");
+  Serial.println("Conectado ao Wi-Fi");
 
-  conecta_firebase();  // Chama a função que irá conectar no FIREBASE
+  conecta_firebase();  // Chama a função que irá conectar no Firebase
 
-  SPI.begin();         // Init SPI bus
-  mfrc522.PCD_Init();  // Init MFRC522
+  SPI.begin();         // Inicia o barramento SPI
+  mfrc522.PCD_Init();  // Inicia o MFRC522
 
-  //Setando os pinos
-  pinMode(FechaduraPinA, OUTPUT);       // Configura D1 como saida
-  pinMode(FechaduraPinB, OUTPUT);       // Configura D2 como saida
-  pinMode(Sensor_Porta, INPUT_PULLUP);  //Configura D4 como entrada
-  pinMode(Campainha, INPUT_PULLUP);     //Configura D3 como entrada
+  // Configuração dos pinos
+  pinMode(FechaduraPinA, OUTPUT);       // Configura D1 como saída
+  pinMode(FechaduraPinB, OUTPUT);       // Configura D2 como saída
+  pinMode(Sensor_Porta, INPUT_PULLUP);  // Configura D4 como entrada
+  pinMode(Campainha, INPUT_PULLUP);     // Configura D3 como entrada
 
-  //Inicializando os pinos
-  digitalWrite(FechaduraPinA, LOW);  //Configuração para fechar a fechadura
-  digitalWrite(FechaduraPinB, LOW);  //Configuração para fechar a fechadura
+  // Inicialização dos pinos
+  digitalWrite(FechaduraPinA, LOW);  // Configuração para fechar a fechadura
+  digitalWrite(FechaduraPinB, LOW);  // Configuração para fechar a fechadura
 
   delay(1000);
 }
 
 void loop() {
-
+  // Verifica se a campainha foi acionada
   if (digitalRead(Campainha)) {
-
     Serial.println("Tem gente na porta");
-
     delay(3000);
   }
 
+  // Verifica se o Firebase está pronto
   if (Firebase.ready()) {
+    // Verifica o valor de abrePorta
+    verifica_abrePorta();
 
-    // Look for new cards
+    // Verifica se há um novo cartão RFID presente
     if (mfrc522.PICC_IsNewCardPresent()) {
-
-      // Select one of the cards
+      // Lê o cartão RFID
       if (!mfrc522.PICC_ReadCardSerial()) {
         return;
       }
 
       String leitura = "";
 
-      // Show UID on serial monitor
+      // Exibe o UID no monitor serial
       Serial.print("UID da tag: ");
       for (byte i = 0; i < mfrc522.uid.size; i++) {
         if (i == 0)
@@ -136,31 +124,28 @@ void loop() {
 
       Serial.println(leitura);
 
-      // Halt PICC
+      // Interrompe o PICC
       mfrc522.PICC_HaltA();
 
-      // Stop encryption on PCD
+      // Para a criptografia no PCD
       mfrc522.PCD_StopCrypto1();
 
       String nodePath = "/ChavesCadastradas/" + leitura;
 
-      // Serial.println("Path: " + nodePath);
-
+      // Verifica se o código do cartão está no Firebase
       if (Firebase.RTDB.getJSON(&fbdo, nodePath)) {
         Serial.println("Código encontrado no Firebase!");
         abre_fechadura();
       } else {
         Serial.println("Código não encontrado no Firebase.");
-        // Serial.println(fbdo.errorReason());
+        Serial.println(fbdo.errorReason());
       }
 
       delay(5000);
 
       while (portaIsFechada) {
-
         // Confere se a porta está fechada | 0 é fechada | 1 é aberta
         portaIsFechada = digitalRead(Sensor_Porta);
-
         delay(1000);
       }
 
@@ -172,49 +157,46 @@ void loop() {
 // Funções
 
 void conecta_wifi() {
+  WiFi.begin(ssid, password);  // Inicia a conexão Wi-Fi
 
-  WiFi.begin(ssid, password);  //Inicia a conexão Wi-Fi
+  Serial.print("Conectando a ");  // Escreve o texto na serial
+  Serial.print(ssid);             // Escreve o nome da rede na serial
 
-  Serial.print("Conectando a ");  //ESCREVE O TEXTO NA SERIAL
-  Serial.print(ssid);             //ESCREVE O NOME DA REDE NA SERIAL
-
-  while (WiFi.status() != WL_CONNECTED) {  //ENQUANTO STATUS FOR DIFERENTE DE CONECTADO
-
+  while (WiFi.status() != WL_CONNECTED) {  // Enquanto status for diferente de conectado
     delay(500);
     Serial.print(".");
   }
 
-  Serial.println("");                         //PULA UMA LINHA NA JANELA SERIAL
-  Serial.print("Conectado a rede sem fio ");  //ESCREVE O TEXTO NA SERIAL
-  Serial.println(ssid);                       //ESCREVE O NOME DA REDE NA SERIAL
+  Serial.println("");                         // Pula uma linha na janela serial
+  Serial.print("Conectado a rede sem fio ");  // Escreve o texto na serial
+  Serial.println(ssid);                       // Escreve o nome da rede na serial
 }
 
 void conecta_firebase() {
-
-  // Configuração do fb no void setup() - >
+  // Configuração do Firebase
   Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
 
-  /* Assign the api key (required) */
+  /* Assign the API key (required) */
   config.api_key = API_KEY;
 
-  /* Assign the user sign in credentials */
+  /* Assign the user sign-in credentials */
   auth.user.email = USER_EMAIL;
   auth.user.password = USER_PASSWORD;
 
   /* Assign the RTDB URL (required) */
   config.database_url = DATABASE_URL;
 
-  /* Assign the callback function for the long running token generation task */
-  config.token_status_callback = tokenStatusCallback;  // see addons/TokenHelper.h
+  /* Assign the callback function for the long-running token generation task */
+  config.token_status_callback = tokenStatusCallback;  // veja addons/TokenHelper.h
 
-  // Comment or pass false value when WiFi reconnection will control by your code or third party library e.g. WiFiManager
+  // Comente ou passe o valor falso quando a reconexão do Wi-Fi for controlada pelo seu código ou por uma biblioteca de terceiros, por exemplo, WiFiManager
   Firebase.reconnectNetwork(true);
 
-  // Large data transmission may require larger RX buffer, otherwise connection issue or data read time out can be occurred.
-  // Provide both RX and TX buffer sizes
-  fbdo.setBSSLBufferSize(4096, 4096); /* Rx buffer size in bytes from 512 - 16384 /, 1024 / Tx buffer size in bytes from 512 - 16384 */
+  // A transmissão de grandes dados pode exigir um buffer de RX maior, caso contrário, problemas de conexão ou tempo limite de leitura de dados podem ocorrer.
+  // Forneça tamanhos de buffer RX e TX
+  fbdo.setBSSLBufferSize(4096, 4096); /* Tamanho do buffer Rx em bytes de 512 - 16384 /, 1024 / Tamanho do buffer Tx em bytes de 512 - 16384 */
 
-  // Limit the size of response payload to be collected in FirebaseData
+  // Limita o tamanho do payload da resposta a ser coletado no FirebaseData
   fbdo.setResponseSize(2048);
 
   Firebase.begin(&config, &auth);
@@ -225,51 +207,37 @@ void conecta_firebase() {
 }
 
 void abre_fechadura() {
-
-  digitalWrite(FechaduraPinA, HIGH);  //Configuração para abrir a fechadura
-  // firebase.setString("isTrancada", "false");
+  Serial.println("Abrindo fechadura...");
+  digitalWrite(FechaduraPinA, HIGH);  // Configuração para abrir a fechadura
 }
 
 void fecha_fechadura() {
-
-  digitalWrite(FechaduraPinA, LOW);  //Configuração para fechar a fechadura
-  // firebase.setString("isTrancada", "true");
+  Serial.println("Fechando fechadura...");
+  digitalWrite(FechaduraPinA, LOW);  // Configuração para fechar a fechadura
 }
 
-// Código pra reutilizar
+void verifica_abrePorta() {
+  Serial.println("Verificando abrePorta...");
+  if (Firebase.RTDB.getInt(&fbdo, "/abrePorta")) {
+    int abrePorta = fbdo.intData();
+    Serial.print("Valor de abrePorta: ");
+    Serial.println(abrePorta);
+    if (abrePorta == 1) {
+      Serial.println("abrePorta é 1, abrindo fechadura...");
+      abre_fechadura();
 
-// // Confere se a porta está fechada | 0 é fechada | 1 é aberta
-//   portaIsFechada = digitalRead(Sensor_Porta);
-//   // Serial.println(portaIsFechada);
+      // Espera até que a porta seja fechada antes de trancar a fechadura novamente
+      while (portaIsFechada) {
+        portaIsFechada = digitalRead(Sensor_Porta);
+        delay(1000);
+      }
 
-//   // Look for new cards
-//   if (mfrc522.PICC_IsNewCardPresent()) {
-
-//     // Select one of the cards
-//     if (!mfrc522.PICC_ReadCardSerial()) {
-//       return;
-//     }
-
-//     String leitura = "";
-
-//     // Show UID on serial monitor
-//     Serial.print("UID da tag: ");
-//     for (byte i = 0; i < mfrc522.uid.size; i++) {
-//       if (i == 0)
-//         leitura.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : ""));
-//       else
-//         leitura.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-
-//       leitura.concat(String(mfrc522.uid.uidByte[i], HEX));
-//     }
-
-//     leitura.toUpperCase();
-
-//     Serial.println(leitura);
-
-//     // Halt PICC
-//     mfrc522.PICC_HaltA();
-
-//     // Stop encryption on PCD
-//     mfrc522.PCD_StopCrypto1();
-//   }
+      //
+    } else {
+      fecha_fechadura();
+    }
+  } else {
+    Serial.println("Falha ao obter o valor de abrePorta do Firebase.");
+    Serial.println(fbdo.errorReason());
+  }
+}
